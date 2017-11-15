ff16c5466c61cc6c749d89cb66374c685996b0cb
jackhowa <jackhowaa@gmail.com>
2017-11-15 08:11:40 -0600

    Add comment on split, brainstorm matching via non-greedy regex

--- pig-latin.js
+++ pig-latin.js
@@ -1,7 +1,13 @@
 function translatePigLatin(str) {
-  const splitStr = str.split("");
-  // const firstConsonantIndex = str.indexOf()
-  // const noFirstConsonant = 
+  const splitStr = str.split(""); // [ 'c', 'o', 'n', 's', 'o', 'n', 'a', 'n', 't' ]
+
+  // need to make the match non-greedy 
+  // via https://stackoverflow.com/questions/2503413/regular-expression-to-stop-at-first-match
+
+  // this is a string method match though 
+  // and returns an array
+  const firstConsonant = str.match();
+  return splitStr;
 }
 
 const output = translatePigLatin("consonant");
