6b3bc4f8684cfe4516097aea7acd86d6dbe97b27
jackhowa <jackhowaa@gmail.com>
2017-11-24 11:48:58 -0600

    Recursive solution with times check

--- pig-latin.js
+++ pig-latin.js
@@ -1,33 +1,44 @@
 /*jshint esversion: 6 */
 
-function translatePigLatin(str) {
+function translatePigLatin(str, times = 0) {
   // check for leading vowel
   const vowelRegex = /([aeiou])/;
-  const firstVowelIndex = str.search(vowelRegex);
+  let firstVowelIndex = str.search(vowelRegex);
 
-  if (firstVowelIndex == 0) {
+  // check for first time vowel 
+  if (firstVowelIndex == 0 && times === 0) {
     return str + "way";
   }
 
-  // work on consonants
-  let checkForConsonants = true;
-  while (checkForConsonants === true) {
-    const consonantsRegex = /([b-df-hj-np-tv-z](.*))/; // list all of the consonants range
-    let firstConsonantIndex = str.search(consonantsRegex); // 0
-    let firstConsonant = str.substr(firstConsonantIndex, 1); // c
-
-    // now I need to delete the first index
-    let withoutFirstConsonant = str.slice(firstConsonantIndex + 1);
+  // recursion end case 
+  if (firstVowelIndex === 0) {
+    return str + "ay";
   }
 
-  // also I need to add it to the end with ay
-  return withoutFirstConsonant + firstConsonant + "ay"; //"on"
+  const consonantsRegex = /([b-df-hj-np-tv-z](.*))/; // list all of the consonants range
+  let firstConsonantIndex = str.search(consonantsRegex); // 0
+  let firstConsonant = str.substr(firstConsonantIndex, 1); // c
+
+  // now I need to delete the first index
+  let withoutFirstConsonant = str.slice(firstConsonantIndex + 1);
+
+  let newStr = withoutFirstConsonant + firstConsonant; //"on"
+
+  times += 1;
+
+  return translatePigLatin(newStr, times);
+
 }
 
-const singleCons = translatePigLatin("consonant");
-console.log("one vowel:");
-console.log(singleCons); // "aliforniacay"
+// const singleCons = translatePigLatin("consonant");
+// console.log("one vowel:");
+// console.log(singleCons); // "aliforniacay"
 
-const leadingVowel = translatePigLatin("eight");
-console.log("empty");
-console.log(leadingVowel);
+// const leadingVowel = translatePigLatin("eight");
+// console.log("empty");
+// console.log(leadingVowel);
+
+
+const doubleCons = translatePigLatin("glove");
+console.log("double cons");
+console.log(doubleCons);
